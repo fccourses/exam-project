d609bb082f5534e873ce07b8375c3dc52241f646
Umar Khalilov <ERMASTER100@gmail.com>
2021-04-28 16:05:10 +0300

    Add fix anchors

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import LoginForm from '../../components/LoginForm/LoginForm';
-import Logo from '../../components/Logo';
-import styles from './LoginPage.module.sass';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { clearErrorSignUpAndLogin } from '../../actions/actionCreator';
-import CONSTANTS from '../../constants';
+import React from 'react'
+import LoginForm from '../../components/LoginForm/LoginForm'
+import Logo from '../../components/Logo'
+import styles from './LoginPage.module.sass'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { clearErrorSignUpAndLogin } from '../../actions/actionCreator'
+import CONSTANTS from '../../constants'
 
-const LoginPage = (props) => {
+const LoginPage = props => {
   const changeRoute = () => {
-    props.history.replace('/');
-  };
+    props.history.replace('/')
+  }
   return (
-    <div className={ styles.mainContainer }>
-      <div className={ styles.loginContainer }>
-        <div className={ styles.headerSignUpPage }>
-          <Logo src={ `${ CONSTANTS.STATIC_IMAGES_PATH }logo.png` } alt="logo"/>
-          <div className={ styles.linkLoginContainer }>
-            <Link to='/registration'
-                  style={ {textDecoration: 'none'} }><span>Signup</span></Link>
-          </div>
+    <div className={styles.mainContainer}>
+      <div className={styles.loginContainer}>
+        <div className={styles.headerSignUpPage}>
+          <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} alt='logo' />
+          <Link to='/registration' style={{ textDecoration: 'none' }}>
+            <div className={styles.linkLoginContainer}>
+              <span>Signup</span>
+            </div>
+          </Link>
         </div>
-        <div className={ styles.loginFormContainer }>
-          <LoginForm changeRoute={ changeRoute }/>
+        <div className={styles.loginFormContainer}>
+          <LoginForm changeRoute={changeRoute} />
         </div>
       </div>
     </div>
-  );
+  )
+}
 
-};
-
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    clearError: () => dispatch(clearErrorSignUpAndLogin()),
-  };
-};
+    clearError: () => dispatch(clearErrorSignUpAndLogin())
+  }
+}
 
-export default connect(null, mapDispatchToProps)(LoginPage);
+export default connect(null, mapDispatchToProps)(LoginPage)
