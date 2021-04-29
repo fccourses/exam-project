2b9f7bee348ca00f1c52f6d3e880641c1bbd1f7b
Umar Khalilov <ERMASTER100@gmail.com>
2021-04-29 12:25:35 +0300

    Add fix warning styles

--- start-dev.sh
+++ start-dev.sh
@@ -13,12 +13,12 @@ echo ""
 
 web=$(docker-compose --file docker-compose-dev.yaml ps | grep exam-project_server-dev_1 | awk '{print $1}')
 
-# Run Sequalize's migrations.
+# Run Sequelize's migrations.
 echo "-----> Running application migrations"
 docker exec -it "$web" sequelize db:migrate
 echo ""
 
-# Run Sequalize's seeds.
+# Run Sequelize's seeds.
 echo "-----> Running application seeds"
 docker exec -it "$web" sequelize db:seed:all
 echo "<----- Seeds created"
